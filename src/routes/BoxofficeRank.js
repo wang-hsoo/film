7dc7ad1cc495e2f476d86cc91bcefb0bdbd1e50b
wang-hsoo <dhkd4873@naver.com>
2022-03-11 02:37:46 +0900

    film 1.1

--- src/routes/BoxofficeRank.js
+++ src/routes/BoxofficeRank.js
@@ -3,46 +3,78 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 
 function BoxofficeRanck(){
-    const [getDate, setDate] = useState(null);
+    const [getDate, setDate] = useState("");
     const [movie, setMovie] = useState([]);
 
-    useEffect(() => {
-        let now = new Date;
-        let year = now.getFullYear();
-        let month = now.getMonth() + 1 > 10 ? now.getMonth()+1  : `0`+(now.getMonth()+1);
-        let day = now.getDay() > 10 ? now.getDay()  : `0`+now.getDay();
-
-        let today = `${year}${month}${day}`;
-
-        setDate(today);
-    },[]);
-
     
+    
+
     const getmovie = async() =>{
         const json = await(
             await fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=d57f7dfe38d2def2190a41bfc774f8b6&targetDt=${getDate}`)
         ).json();
 
-        setMovie(json.boxOfficeResult.daliyBoxOfficeList);
+        setMovie(json.boxOfficeResult.dailyBoxOfficeList);
+        console.log(json.boxOfficeResult.dailyBoxOfficeList);
+    
     }
 
     useEffect(() => {
-        getmovie();
-    },[]);
-    
+        var now = new Date;
+        var year = now.getFullYear();
+        var month = now.getMonth() + 1 > 10 ? now.getMonth()+1  : `0`+(now.getMonth()+1);
+        var day = now.getDate() > 10 ? now.getDate()  : `0`+ now.getDate();
 
+        let today = `${year}${month}${day}`;
+
+        setDate(today);
+        getmovie();
+        console.log(today);
+    },[]);
+
+    const Datepick = () => {
+        const [startDate, setStartDate] = useState(new Date());
+        return (
+          <DatePicker
+            dateFormat="yyyy/MM/dd"
+            selected={startDate}
+            onChange={(date) => {
+                var year = date.getFullYear();
+                var month = date.getMonth() + 1 > 10 ? date.getMonth()+1  : `0`+(date.getMonth()+1);
+                var day = date.getDate() > 10 ? date.getDate()  : `0`+ date.getDate();
+
+                let today = `${year}${month}${day}`;
+                setStartDate(date);
+                setDate(today);
+                getmovie();
+                
+            }}
+          />
+        );
+      };
+
+  
+    
+    
 
     return(
         <div>
             <h3>boxofficeRank</h3>
+            <Datepick />
 
             {movie.map((movies) => {
-                <ul>
-                    <li>{movies.rank}</li>
-                    <li>{movies.movieNm}</li>
-                    <li>{movies.audiAcc}</li>
-                </ul>
+                return(
+                    <div key = {movies.movieCd}>
+                        <div>{getDate}</div>
+                        <div>{movies.rank}</div>
+                        <div>{movies.movieNm}</div>
+                        <div>{movies.audiAcc}</div>
+                    </div>
+                    );
             })}
+
+            
+
         </div>
     );
 
