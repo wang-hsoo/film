b4e76bf3bd397f3587b488f3c05612e7d5560a5e
wang-hsoo <dhkd4873@naver.com>
2022-03-30 01:57:36 +0900

    film 1.2

---
+++ client/src/component/Trailer.js
@@ -0,0 +1,11 @@
+function Trailer(cgv) {
+
+    console.log(cgv);
+    return(
+        <div>
+            <div>예고편</div>
+        </div>
+    )
+}
+
+export default Trailer;
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import { Link } from "react-router-dom";
+import Trailer from "../component/Trailer";
+import style from "./Home.module.css";
 
 function Home(){
     let moviesA = [];
@@ -39,8 +41,6 @@ function Home(){
                 }
             })
         })
-
-        console.log(movie.cgv);
         
 
     }
@@ -61,8 +61,8 @@ function Home(){
             {loading ? 
               <> 
                 <header>
-                    <h2 style ={{'color' : '#f5c510'}}>Film</h2>
-                    <Link to={'/Login'}>Login</Link>
+                    <h2>Film</h2>
+                    <Link to={'/Login'} className = {style.loginBtn}>Login</Link>
                 </header>
                 <main>
                     <div className="slide-container">
@@ -75,22 +75,32 @@ function Home(){
                         </Slide>
                     </div>
 
-                    <div>
+                    <div className={style.top_movie}>
                         <h3>현재 상영중인 영화</h3>
-                        <div>
+                        <div className={style.allBtn}>모두보기</div>
+                        <div className={style.top_movie_group}>
                             <ul>
                             {moviesA?.map( (movies) => (
 
-                                <li key = {movies.key} style={{display: "inline-block"}}>
-                                    <span>{movies.title}</span>
-                                    <img src={movies.img} width="100px"></img>
-                                    <span>{movies.percent}</span>
-                                    <span>{movies.open}</span>
-                                    <span>{movies.age}</span>
+                                <li key = {movies.key} className = {style.top_movie_li}>
+                                    <span className = {style.top_movie_li_title}>{movies.title}</span>
+                                    <img src={movies.img}></img>
+                                    <span className = {style.top_movie_li_percent}>{movies.percent}</span>
+                                    <span  className = {style.top_movie_li_open}>{movies.open}</span>
+                                    <span className = {style.top_movie_li_age}>{movies.age}</span>
+                                    {/* 구성에 따라 순서 바꿔도 무관 */}
                                 </li>
 
                                 ))}
                             </ul>
+                        </div>
+                    </div>
+
+                    <div className={style.trailer}>
+                        <h3>트레일러</h3>
+                        <div className={style.trailer_group}>
+                            {/* Trailer.js 파일로 가면됨 component 폴더 밑에 있음 */}
+                            <Trailer movie = {movie.cgv} />
                         </div>
                     </div>
                 </main>
